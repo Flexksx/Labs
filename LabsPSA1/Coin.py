77df8ee836d843c5a01a872af046fb3dd94ee9f8
Flexksx <113352531+Flexksx@users.noreply.github.com>
2022-11-28 09:41:11 +0200

    Create Coin.py

---
+++ LabsPSA1/Coin.py
@@ -0,0 +1,22 @@
+import random
+
+
+def flip():
+    wins = 0
+    coin = 1
+    while coin == 1:
+        wins += 1
+        coin = random.uniform(0, 1)
+        if coin<0.5:
+            coin=0
+        else:
+            coin=1
+    return pow(2, wins)
+
+
+res = 0
+tries = 1000000
+for i in range(tries):
+    res += flip()
+
+print(res / tries)
