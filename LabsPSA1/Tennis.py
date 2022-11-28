e41b04313e48dab619d395ed2f28f8d23eaf5800
Flexksx <113352531+Flexksx@users.noreply.github.com>
2022-11-28 09:42:01 +0200

    Create Tennis.py

---
+++ LabsPSA1/Tennis.py
@@ -0,0 +1,34 @@
+import random
+
+def game():
+	anascore=0
+	danscore=0
+	anaturn=1
+	anawin=0
+	while(anascore<=25 and danscore<=25):
+		while anaturn:
+			x=random.uniform(0,1)
+			if(x>=0.3):
+				anascore+=1
+			else:
+				anaturn=0
+				danscore+=1
+			if anascore>=25:
+				return 1
+			
+		while not anaturn:
+			x=random.uniform(0,1)
+			if(x>=0.5):
+				danscore+=1
+			else:
+				anaturn=1
+				anascore+=1
+			if danscore>=25:
+				return 0
+
+tries=100000
+anawins=0
+for i in range(tries):
+	anawins+=game()
+print("Probability of Ana win:",anawins/tries)
+
