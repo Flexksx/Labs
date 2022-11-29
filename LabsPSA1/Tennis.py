e3ada14b76ea91054afaf843c0d46b164e4054b2
Flexksx <113352531+Flexksx@users.noreply.github.com>
2022-11-29 12:57:47 +0200

    zxc

--- LabsPSA1/Tennis.py
+++ LabsPSA1/Tennis.py
@@ -1,33 +1,37 @@
 import random
 
+
 def game():
-	anascore=0
-	danscore=0
-	anaturn=1
-	while(anascore<=25 and danscore<=25):
-		while anaturn:
-			x=random.uniform(0,1)
-			if(x>=0.3):
-				anascore+=1
-			else:
-				anaturn=0
-				danscore+=1
-			if anascore>=25:
-				return 1
-			
-		while not anaturn:
-			x=random.uniform(0,1)
-			if(x>=0.5):
-				danscore+=1
-			else:
-				anaturn=1
-				anascore+=1
-			if danscore>=25:
-				return 0
+    ana = [True, False]
+    dan = [True, False]
+    anascore = 0
+    danscore = 0
+    anaturn = 1
+    while anascore <= 25 and danscore <= 25:
+        while anaturn:
+            x = random.choices(ana, weights=(70, 30), k=1)
+            if x[0]:
+                anascore += 1
+            else:
+                anaturn = 0
+                danscore += 1
+            if anascore >= 25:
+                return 1
 
-tries=100000
-anawins=0
+        while not anaturn:
+            x = random.choices(dan, k=1)
+            if x[0]:
+                danscore += 1
+            else:
+                anaturn = 1
+                anascore += 1
+        if danscore >= 25:
+            return 0
+
+
+tries = 10000
+anawins = 0
 for i in range(tries):
-	anawins+=game()
-print("Probability of Ana win:",anawins/tries)
+    anawins += game()
 
+print("Probability of Ana win:", anawins / tries)
