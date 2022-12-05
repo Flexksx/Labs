819b37b6c46ffe0cd8a7293e2e83c4939c6867df
Flexksx <113352531+Flexksx@users.noreply.github.com>
2022-12-05 14:45:31 +0200

    zxc

---
+++ LabsPSA2/problem1.py
@@ -0,0 +1,29 @@
+import random
+
+
+def dice():
+    return random.randint(1, 6)
+
+
+def prob9(tries):
+    prob=0
+    for i in range(tries):
+        roll1 = dice()
+        roll2 = dice()
+        if(roll1+roll2==9):
+          prob+=1
+    return prob/tries
+
+def prob10(tries):
+    prob=0
+    for i in range(tries):
+        roll1=dice()
+        roll2=dice()
+        roll3=dice()
+        if(roll1+roll2+roll3==10):
+            prob+=1
+    return prob/tries
+
+tries=100000
+print(prob9(tries))
+print(prob10(tries))
---
+++ LabsPSA2/problem2.py
@@ -0,0 +1,25 @@
+import random
+
+
+def do():
+    x1 = random.uniform(0, 1)
+    x2 = random.uniform(0, 1)
+    if x1 > x2:
+        temp = x1
+        x1 = x2
+        x2 = temp
+    a = x1
+    b = x2 - x1
+    c = 1 - (a + b)
+    if a + b > c and b + c > a and a + c > b:
+        return 1
+    else:
+        return 0
+
+
+tries = 1000000
+prob = 0
+for i in range(tries):
+    prob += do()
+
+print(prob / tries)
