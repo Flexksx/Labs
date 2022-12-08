1aa0ead1c6e17b18cc78d02c077ce0560b45d204
Flexksx <113352531+Flexksx@users.noreply.github.com>
2022-12-08 19:01:04 +0200

    update

--- Labs2/LabsPSA2/problem5.py
+++ Labs2/LabsPSA2/problem5.py
@@ -1,28 +1,34 @@
 import random
 from numpy import sqrt
 
+
 def equation():
     ans = []
     b = random.uniform(-1, 1)
     c = random.uniform(-1, 1)
     if (pow(b, 2) - 4 * c) < 0:
-        ans.append("Complex")
+        ans = ["C", "N"]
+        return ans
     else:
-        ans.append("Real")
-    x1 = (-b + sqrt(pow(b, 2) - 4 * c)) / 2
-    x2 = (-b - sqrt(pow(b, 2) - 4 * c)) / 2
-    if ans[0] == "Real" and x1 > 0 and x2 > 0:
-        ans.append("Positive")
+        ans.append("R")
+    x1 = (-b + pow((pow(b, 2) - 4 * c), 0.5)) / 2
+    x2 = (-b - pow((pow(b, 2) - 4 * c), 0.5)) / 2
+    if x1 > 0 and x2 > 0:
+        ans.append("P")
     else:
-        ans.append("Negative")
+        ans.append("N")
     return ans
 
-tries=10000
-real=0
-positive=0
+
+tries = 100000
+real = 0
+positive = 0
 for i in range(tries):
-    roots=equation()
-    if roots[0]=="Real":
-        real+=1
+    roots = equation()
+    if roots[0] == "R":
+        real += 1
+    if roots[1] == "P":
+        positive += 1
 
-
+print("Probability of both reals:", real / tries)
+print("Probability of both positive: ", positive / tries)
