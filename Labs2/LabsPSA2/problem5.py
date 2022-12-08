f89ab34926acdaf129bb2c9bcc66d333494b2122
Flexksx <113352531+Flexksx@users.noreply.github.com>
2022-12-08 14:33:19 +0200

    update

--- Labs2/LabsPSA2/problem5.py
+++ Labs2/LabsPSA2/problem5.py
@@ -1,17 +1,28 @@
 import random
+from numpy import sqrt
 
-def coi():
-    ans=[]
-    b=random.uniform(-1,1)
-    c=random.uniform(-1,1)
-    if (pow(b,2)-4*c)<0:
+def equation():
+    ans = []
+    b = random.uniform(-1, 1)
+    c = random.uniform(-1, 1)
+    if (pow(b, 2) - 4 * c) < 0:
         ans.append("Complex")
     else:
         ans.append("Real")
-    x1=(-b+pow((pow(b,2)-4*c),0.5))/2
-    x2=(-b-pow((pow(b,2)-4*c),0.5))/2
-    if x1>0 and x2>0 and ans[0]=="Real":
+    x1 = (-b + sqrt(pow(b, 2) - 4 * c)) / 2
+    x2 = (-b - sqrt(pow(b, 2) - 4 * c)) / 2
+    if ans[0] == "Real" and x1 > 0 and x2 > 0:
         ans.append("Positive")
     else:
         ans.append("Negative")
-    print(ans)
+    return ans
+
+tries=10000
+real=0
+positive=0
+for i in range(tries):
+    roots=equation()
+    if roots[0]=="Real":
+        real+=1
+
+
