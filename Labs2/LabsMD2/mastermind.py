dfb8df9f49d45e9822eb65992c72d8a7a3ec85ed
Flexksx <113352531+Flexksx@users.noreply.github.com>
2022-12-13 20:38:19 +0200

    update

--- Labs2/LabsMD2/mastermind.py
+++ Labs2/LabsMD2/mastermind.py
@@ -1,45 +1,84 @@
 import random
 from tkinter import *
 
-window=Tk()
-window.geometry("500x300")
-window.title("Mastermind")
-label1=Label(window, text="Guess the number!", font=("Times New Roman",32), padx=80)
-label1.grid(column=0, row=0)
-labelnum = Label(window, text="XXXX", font=("Times New Roman", 24), padx=100, pady=100)
-labelnum.grid(column=0, row=1)
+# Greeting stuff
+colors = ['Red', 'Green', 'Yellow', 'Purple', 'Blue', 'Cyan']
+
+window = Tk()
+spinsize = 10
+window.geometry("700x700")
+window.title("MastermindFUCK")
+greetinglabel = Label(window, text="Try to break the code! There are 6 possible colors.", font=("Bahnschrift", 20),
+                      padx=50, pady=10)
+greetinglabel.grid(column=0, row=0)
+
+
+# Greeting stuff
+
+# Player input
+
+def getbox1():
+    global box1
+    box1=StringVar()
+    box1=Spinbox.get()
+
+def getbox2():
+    global box2
+    box2= StringVar()
+    box2=Spinbox.get()
+
+def getbox3():
+    global box3
+    box3= StringVar()
+    box3=Spinbox.get()
+
+def getbox4():
+    global box4
+    box4 = StringVar()
+    box4=Spinbox.get()
+
+
+tryframes = []
+for i in range(12):
+    tryframes.append(Frame(window))
+    tryframes[i].grid(column=0, row=i + 2)
+
+inputframe = Frame(window)
+inputframe.grid(column=0, row=1)
+spinbox1 = Spinbox(inputframe, values=colors, font=("Bahnschrift", spinsize), command=getbox1()).pack(side=RIGHT)
+spinbox2 = Spinbox(inputframe, values=colors, font=("Bahnschrift", spinsize), command=getbox2()).pack(side=RIGHT)
+spinbox3 = Spinbox(inputframe, values=colors, font=("Bahnschrift", spinsize), command=getbox3()).pack(side=RIGHT)
+spinbox4 = Spinbox(inputframe, values=colors, font=("Bahnschrift", spinsize), command=getbox4()).pack(side=RIGHT)
+
+trybutton = Button(inputframe, text="TRY!", font=("Bahnschrift", spinsize)).pack(side=LEFT)
+
+# Player input
+
+
 window.mainloop()
 
 
-num = str(random.randint(1000,9999))
-def getblank(num):
-    show=[]
-    for i in range(len(num)):
-        show.append('X')
-    return ''.join(show)
+def getcode(colors):
+    return random.choices(colors, k=4)
 
-show=getblank(num)
 
-gamegoing=True
-steps=0
-print(num)
-while gamegoing:
-    show=getblank(num)
-    x = input()
+code = getcode(colors)
+
+
+def givepins(code, x):
+    pins = []
+    pos = []
     for i in range(len(x)):
-        if num[i]==x[i]:
-            show=list(show)
-            show[i]=num[i]
-            show=''.join(show)
-    print(show)
-
-    steps+=1
-    if show==num:
-        print("You won in", steps,"steps!")
-        gamegoing=False
-    if x=='stop':
-        gamegoing=False
+        if x[i] == code[i]:
+            pins.append('R')
+            x[i] = 'X'
+    x = list(x)
+    for i in x:
+        if i in code:
+            pins.append('W')
+            i = 'X'
+    print(x)
+    return pins
 
 
-
-
+print(code)
