fbade90f3464195d886f51ff1461ea84d8c739eb
Flexksx <113352531+Flexksx@users.noreply.github.com>
2022-12-14 22:39:02 +0200

    upd

---
+++ Labs2/LabsPSA2/problem7.py
@@ -0,0 +1,22 @@
+import random
+import matplotlib.pyplot as plt
+
+tries = 1000
+distribution = {}
+
+for i in range(35,65):
+    distribution.update({i:0})
+
+for i in range(tries):
+    heads = 0
+    for j in range(100):
+        if random.randint(0, 1) == 1:
+            heads += 1
+    if heads in distribution:
+        distribution[heads] += 1
+
+x = list(distribution.keys())
+y = list(distribution.values())
+
+plt.bar(x, y)
+plt.show()
