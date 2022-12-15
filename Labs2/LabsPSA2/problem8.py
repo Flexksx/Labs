661db3854c8ab10d6c3bff583e9842d5e0a6c650
Flexksx <113352531+Flexksx@users.noreply.github.com>
2022-12-15 11:00:00 +0200

    upd

--- Labs2/LabsPSA2/problem8.py
+++ Labs2/LabsPSA2/problem8.py
@@ -1,3 +1,26 @@
 import random
 
 n = 10
+participants = []
+
+for i in range(n):
+    participants.append(str(i))
+
+
+def seats(people, n):
+    sequence = []
+    while len(sequence) < n:
+        x = random.randint(0, n - 1)
+        if people[x] not in sequence:
+            sequence.append(people[x])
+    return sequence
+
+
+def compare(people, n):
+    lunch = seats(people, n)
+    dinner = seats(people, n)
+    print(lunch)
+    print(dinner)
+
+
+compare(participants, n)
