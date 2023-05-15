3a82c0c927084a57ae03431ee14b7d42e12a8c41
Flexksx <113352531+Flexksx@users.noreply.github.com>
2023-05-15 12:43:29 +0300

    one more time

---
+++ LabsNA/Labs1NA/sugmalator/sugmalator.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from int_operations import DecPlusInt, DecMinusInt
+# window=Tk()
+# window.geometry("500x300")
+# window.title("Sugmalator")
+# NameFrame=Frame(window)
+# NameFrameLabel=Label(NameFrame, text="Kizda messi", font=("Bahnschrift",20)).pack()
+
+
+    
+
+print(DecPlusInt("212","12"))        
