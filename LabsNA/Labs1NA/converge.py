ad6f60024ecc0abe16fb684265931dd366d05595
Flexksx <113352531+Flexksx@users.noreply.github.com>
2023-05-19 16:38:34 +0300

    WHAT IS BRO DOINNNN??????!!!!

--- LabsNA/Labs1NA/converge.py
+++ LabsNA/Labs1NA/converge.py
@@ -1,24 +1,23 @@
-global TOL
-TOl = 1e-5
-
+import numpy as np
 
 def g(x, c):
-    return 2 * x - c * x * x
+    return 2*x - c*x**2
 
+def fixed_point_iteration(c, initial_guess, num_iterations):
+    p = initial_guess
+    for _ in range(num_iterations):
+        p = g(p, c)
+    return p
 
-def dg(x, c):
-    return 2 - 2 * c * x
+def do(c, initial_guess):
+    num_iterations = 100
+    limit = 1 / c
+    result = fixed_point_iteration(c, initial_guess, num_iterations)
+    print("Limit for c=",c,":", limit)
+    print("Result c=",c,":", result)
 
+'''if we assume that it converges to 1/c, and we test for any c that is a positive integer, we can make the initial guess as 0.1,
+because 0 will be a root of the function'''
 
-def FPINewton(c, guess):
-    xold = guess
-    iter = 0
-    while iter < 100:
-        xnew = xold - g(xold, c)/dg(xold,c)
-        xold = xnew
-        iter += 1
-        if xold==0.0:
-            return xold
-    xnew = xold - g(xold, c)
-    return xnew
-
+for i in range(1,10):
+    do(i,0.1)
